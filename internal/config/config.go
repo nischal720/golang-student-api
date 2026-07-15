378dc196bea907d0eb5a905a11f28152b208bb04
nischal720 <dhakalnischak776@gmail.com>
2026-07-15 16:52:03 +0545

    parse config file

---
+++ internal/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HttpServer struct {
+	Address string
+}
+
+type Config struct {
+	Env         string `yml:"env" env:"ENV" env-required:"true" env-default:"production" `
+	StoragePath string `yml:"storage_path" env-required:"true" `
+	HttpServer  `yml:"http_server"`
+}
+
+func MustLoad() *Config {
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config-path", "", "path to the config file")
+		flag.Parse()
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("Config path is not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+
+		log.Fatalf("cofig path doesnot exit: %s", configPath)
+	}
+
+	var cfg Config
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil {
+		log.Fatalf("canot read config file %s", err.Error())
+	}
+
+	return &cfg
+
+}
